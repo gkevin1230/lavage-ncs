065f9140b8d72ac78b1336b184f14c74258060ef
gkevin1230 <geiger1230@gmail.com>
2023-04-19 16:28:18 +0200

    footer lien base

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <?php if (!$is_home_page): ?>
     <div class="back-home container mx-auto text-center">
-        <a href="/LAVAGE-NCS/" class="text-center inline-flex items-center justify-center p-5 text-base font-medium text-gray-500">
+    <a href="<?php echo dirname($_SERVER['PHP_SELF']); ?>" class="text-center inline-flex items-center justify-center p-5 text-base font-medium text-gray-500">
+
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
             </svg>
